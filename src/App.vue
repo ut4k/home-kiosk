45a834bfffcc97b63a3dd0a3d244bd5ed07d5680
ut4k <aquarium011@gmail.com>
2023-09-10 23:18:48 +0900

    remove vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>おうちコントロールパネル</h1>
+	<div class="home">
+	<img usemap="#interact" src="./assets/room.png">
+		<map name="interact">
+			<area shape="poly" coords="142,71,134,116,32,145,13,141,10,91,124,62,124,62" href="#" alt="" />
+			<area shape="poly" coords="195,98,197,87,209,74,225,72,277,74,294,86,298,99,286,107,270,111,231,113,207,106,207,106" href="#" alt="" />
+			<area shape="poly" coords="359,419,351,278,429,306,465,322,461,436,425,449,360,422,360,422" href="#" alt="" />
+			<area shape="poly" coords="179,294,141,294,100,305,92,313,95,360,123,379,181,358,181,358" href="#" alt="" />
+		</map>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+@font-face {
+    font-family: "pixel";
+    src: url("assets/Nosutaru-dotMPlusH-10-Regular.ttf");
+}
+
+body {
+	background: #292929;
+}
+
+h1 {
+	font-family: "pixel";
+	color: #ff69fa;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+img     {
+  image-rendering: pixelated;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: crisp-edges;
 }
 </style>
